58100991d444e99766adecc62e6de6e2c3deea26
kakafajar <161531668+kakafajar@users.noreply.github.com>
2024-10-03 11:01:15 +0700

    Update know.py

--- ubp_techno_club/know.py
+++ ubp_techno_club/know.py
@@ -12,11 +12,10 @@ arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 Output:
 [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 """
+arr = range(1, 21)
 
-arr = range(1, 100)
-
-# Menggunakan list comprehension untuk menyaring angka ganjil
+# Menggunakan list comprehension untuk memfilter angka ganjil
 ganjil = [x for x in arr if x % 2 != 0]
 
+# Menampilkan hasil
 print(ganjil)
-
